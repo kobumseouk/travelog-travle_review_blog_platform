75154eefc882dda812ed66bf749679841eb2fe81
thoon <131009655+ChatHTH@users.noreply.github.com>
2024-08-19 18:38:59 +0900

    초기 세팅

---
+++ src/main/java/cloud4/team4/travelog/TravelogApplication.java
@@ -0,0 +1,13 @@
+package cloud4.team4.travelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelogApplication.class, args);
+	}
+
+}
