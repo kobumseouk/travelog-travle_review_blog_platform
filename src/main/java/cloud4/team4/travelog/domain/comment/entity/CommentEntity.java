4f5b19c8a0b7f2d4f70cdc57acb3a8db386d2d44
thoon <131009655+ChatHTH@users.noreply.github.com>
2024-08-19 18:57:20 +0900

    style: 기본 디렉토리 구조 생성

---
+++ src/main/java/cloud4/team4/travelog/domain/comment/entity/CommentEntity.java
@@ -0,0 +1,4 @@
+package cloud4.team4.travelog.domain.comment.entity;
+
+public class CommentEntity {
+}
---
+++ src/main/java/cloud4/team4/travelog/domain/post/service/PostService.java
@@ -0,0 +1,4 @@
+package cloud4.team4.travelog.domain.post.service;
+
+public class PostService {
+}
