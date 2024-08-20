b1c8faa1d414fda51f004f78f79782ee81c90df0
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-08-20 16:42:36 +0900

    refactor: comment 엔티티 변경 @Setter 추가 (mapper 때문)

--- src/main/java/cloud4/team4/travelog/domain/comment/entity/Comment.java
+++ src/main/java/cloud4/team4/travelog/domain/comment/entity/Comment.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comment {
