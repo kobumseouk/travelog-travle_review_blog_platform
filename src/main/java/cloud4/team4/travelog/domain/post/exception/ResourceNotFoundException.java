a0bd0709f9511f057bb4946a2edb9b764295cfe6
[CLOUD4] 현태훈 <winwin_th@naver.com>
2024-08-22 11:38:48 +0000

    Revert "Merge branch 'post_photo_feature' into 'master'"
    This reverts merge request !9

--- src/main/java/cloud4/team4/travelog/domain/post/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cloud4.team4.travelog.domain.post.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) {
-    super(message);
-  }
-}
