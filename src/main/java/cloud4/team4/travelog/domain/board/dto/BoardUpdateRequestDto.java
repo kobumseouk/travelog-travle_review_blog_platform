f09cd987fbb1ec6eeaa5e87050ecb89223333e58
thoon <131009655+ChatHTH@users.noreply.github.com>
2024-08-23 19:21:47 +0900

    fix: update Mapper 미사용으로 복구

--- src/main/java/cloud4/team4/travelog/domain/board/dto/BoardUpdateRequestDto.java
+++ src/main/java/cloud4/team4/travelog/domain/board/dto/BoardUpdateRequestDto.java
@@ -1,13 +1,12 @@
 package cloud4.team4.travelog.domain.board.dto;
 
-import cloud4.team4.travelog.domain.board.entity.Board;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class BoardUpdateRequestDto {
     private String description;
-
-    public BoardUpdateRequestDto(Board board) {
-        this.description = board.getDescription();
-    }
 }
--- src/main/java/cloud4/team4/travelog/domain/board/dto/BoardUpdateResponseDto.java
+++ src/main/java/cloud4/team4/travelog/domain/board/dto/BoardUpdateResponseDto.java
@@ -3,8 +3,10 @@ package cloud4.team4.travelog.domain.board.dto;
 import cloud4.team4.travelog.domain.board.entity.Board;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class BoardUpdateResponseDto {
     private final String description;
 
