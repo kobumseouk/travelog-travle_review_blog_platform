1525d01bfc49838bec77017b2d0830e1f1c8c3a7
[CLOUD4] 고범석 <kbssky94@gmail.com>
2024-08-23 18:59:48 +0900

    feat: MapperImpl 커밋 내역에서 무시

--- src/main/generated/cloud4/team4/travelog/domain/board/entity/BoardMapperImpl.java
+++ src/main/generated/cloud4/team4/travelog/domain/board/entity/BoardMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-23T17:17:35+0900",
+    date = "2024-08-23T18:55:06+0900",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
 )
 @Component
--- src/main/generated/cloud4/team4/travelog/domain/comment/dto/CommentMapperImpl.java
+++ src/main/generated/cloud4/team4/travelog/domain/comment/dto/CommentMapperImpl.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-23T17:17:35+0900",
+    date = "2024-08-23T18:55:06+0900",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
 )
 public class CommentMapperImpl implements CommentMapper {
