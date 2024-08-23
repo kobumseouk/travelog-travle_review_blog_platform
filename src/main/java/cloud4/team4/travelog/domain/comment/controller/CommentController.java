43b64deb1e36d27c74e1286df41faf87462c48f2
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-08-23 14:12:02 +0900

    feat: 댓글(게시글 포함) 화면, 컨트롤러 생성

--- src/main/java/cloud4/team4/travelog/domain/comment/controller/CommentController.java
+++ src/main/java/cloud4/team4/travelog/domain/comment/controller/CommentController.java
@@ -1,4 +1,37 @@
 package cloud4.team4.travelog.domain.comment.controller;
 
+
+import cloud4.team4.travelog.domain.comment.dto.CommentRequestDto;
+import cloud4.team4.travelog.domain.comment.service.CommentService;
+import cloud4.team4.travelog.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.jsf.el.SpringBeanFacesELResolver;
+
+@Controller
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final PostService postService;
+
+    @GetMapping("/post/{postId}")
+    public String post(@PathVariable("postId") Long postId, Model model) {
+
+        model.addAttribute("commentRequestDto", new CommentRequestDto());
+        model.addAttribute("post", postService.getPostById(postId));
+
+        // post만 넘기면 아래는 없어도 됨
+//        model.addAttribute("commentList", commentService.findAllByPostId(postId));
+
+        return "post";
+    }
+
+    @GetMapping("/mainV")
+    public String mainV() {
+        return "main";
+    }
 }
