a516a8424571ac95d3db8bf98f6882ac17afeea1
[CLOUD4] 고범석 <kbssky94@gmail.com>
2024-08-24 19:28:42 +0900

    fix: dev 충돌 수정

--- src/main/java/cloud4/team4/travelog/domain/post/controller/PostViewController.java
+++ src/main/java/cloud4/team4/travelog/domain/post/controller/PostViewController.java
@@ -4,7 +4,7 @@ import cloud4.team4.travelog.domain.board.service.BoardService;
 import cloud4.team4.travelog.domain.comment.dto.CommentRequestDto;
 import cloud4.team4.travelog.domain.comment.service.CommentService;
 import cloud4.team4.travelog.domain.post.dto.PostListViewResponse;
-import cloud4.team4.travelog.domain.post.dto.PostPostDto;
+import cloud4.team4.travelog.domain.post.dto.PostResponseDto;
 import cloud4.team4.travelog.domain.post.dto.PostViewResponse;
 import cloud4.team4.travelog.domain.post.entity.Post;
 import cloud4.team4.travelog.domain.post.service.PostService;
@@ -31,54 +31,49 @@ public class PostViewController {
 
   @GetMapping("/board/posts") // 상세 게시판
   public String listPosts(Model model,
-                          @RequestParam(defaultValue = "1") int postPage,
+                          @RequestParam(defaultValue = "1") int page,
                           @RequestParam(defaultValue = "createdAt") String sortBy) {
 
-    Sort sort = createSort(sortBy);  // 최신순/추천순 값에 따라 정렬
+    Sort sort = createSort(sortBy);
+    Pageable pageable = PageRequest.of(page, 10, sort);
+    Page<Post> postPage = postService.getAllPosts(pageable);
 
-    Pageable pageable = PageRequest.of(postPage, 10, sort);  // 페이징 기능
-    Page<Post> currentPostPage = postService.getAllPosts(pageable);
-
-    Page<PostListViewResponse> postListViewResponsePage = currentPostPage.map(PostListViewResponse::new);
+    Page<PostListViewResponse> postListViewResponsePage = postPage.map(PostListViewResponse::new);
 
     model.addAttribute("posts", postListViewResponsePage);
-    model.addAttribute("currentPage", currentPostPage);
-    model.addAttribute("totalPages", currentPostPage.getTotalPages());
+    model.addAttribute("currentPage", page);
+    model.addAttribute("totalPages", postPage.getTotalPages());
     model.addAttribute("sortBy", sortBy);
 
     return "boardPost";
   }
 
   private Sort createSort(String sortBy) {
-    if ("recommends".equals(sortBy)) {  // 추천순 + 최신순 정렬
-      return Sort.by("recommends").descending().and(Sort.by("createdAt").descending());
-    } else {  // 작성일 기준 최신순 정렬 (기본값)
-      return Sort.by("createdAt").descending();
+    switch (sortBy) {
+      case "recommends":
+        return Sort.by("recommends").descending().and(Sort.by("createdAt").descending());
+      case "createdAt":
+      default:
+        return Sort.by("createdAt").descending();
     }
   }
 
-  @GetMapping("board/posts/{postId}")
-  public String post(@PathVariable("postId") Long postId,
-                     @RequestParam(required = false, value = "commentPage", defaultValue = "1") int commentPage,
-                     Model model) {
+  @GetMapping("board/post/{postId}")
+  public String post(@PathVariable("postId") Long postId, Model model) {
     Post post = postService.getPostById(postId);
 
     model.addAttribute("commentRequestDto", new CommentRequestDto());
     model.addAttribute("post", new PostViewResponse(post));
-    model.addAttribute("comments", commentService.findPagedCommentsByPostId(postId, commentPage));
 
     return "post";
   }
 
-
-
-
-  @GetMapping("board/new-post")
+  @GetMapping("/new-post")
   public String newPost(@RequestParam(required = false, name = "postId") Long postId, Model model) {
     // id 가 없으면 새롭게 블로그를 만든다.
     // id 가 있으면 수정
     if (postId == null) {
-      model.addAttribute("post", new PostPostDto());
+      model.addAttribute("post", new PostViewResponse());
     } else {
       Post post = postService.getPostById(postId); // 조회하고 DTO객체에 감싸서 수정
       model.addAttribute("post", new PostViewResponse(post));
