ebe410572576ab0e50a7112bac7f2de55c7ff7d1
[CLOUD4] 고범석 <kbssky94@gmail.com>
2024-08-26 17:10:04 +0900

    feat: 이미지 저장 경로 DB로 변경 및 리스트에서 이미지 사용 1개 제한

--- src/main/java/cloud4/team4/travelog/domain/post/controller/PostViewController.java
+++ src/main/java/cloud4/team4/travelog/domain/post/controller/PostViewController.java
@@ -4,8 +4,8 @@ import cloud4.team4.travelog.domain.board.service.BoardService;
 import cloud4.team4.travelog.domain.comment.dto.CommentRequestDto;
 import cloud4.team4.travelog.domain.comment.service.CommentService;
 import cloud4.team4.travelog.domain.post.dto.PostListViewResponse;
+import cloud4.team4.travelog.domain.post.dto.PostMapper;
 import cloud4.team4.travelog.domain.post.dto.PostPostDto;
-import cloud4.team4.travelog.domain.post.dto.PostViewResponse;
 import cloud4.team4.travelog.domain.post.entity.Post;
 import cloud4.team4.travelog.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -13,21 +13,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class PostViewController {
-  private final BoardService boardService;
+  // private final BoardService boardService;
   private final PostService postService;
   private final CommentService commentService;
+  private final PostMapper postMapper;
 
   @GetMapping("/board/posts") // 상세 게시판
   public String listPosts(Model model,
@@ -65,7 +64,7 @@ public class PostViewController {
     postService.incrementViews(postId);  // 방문마다 조회수 증가
 
     model.addAttribute("commentRequestDto", new CommentRequestDto());
-    model.addAttribute("post", new PostViewResponse(post));
+    model.addAttribute("post", postMapper.postToPostResponseDto(post));
     model.addAttribute("comments", commentService.findPagedCommentsByPostId(postId, commentPage));
 
     return "post";
@@ -73,13 +72,11 @@ public class PostViewController {
 
   @GetMapping("board/new-post")
   public String newPost(@RequestParam(required = false, name = "postId") Long postId, Model model) {
-    // id 가 없으면 새롭게 블로그를 만든다.
-    // id 가 있으면 수정
     if (postId == null) {
       model.addAttribute("post", new PostPostDto());
     } else {
-      Post post = postService.getPostById(postId); // 조회하고 DTO객체에 감싸서 수정
-      model.addAttribute("post", new PostViewResponse(post));
+      Post post = postService.getPostById(postId);
+      model.addAttribute("post", postMapper.postToPostResponseDto(post));
     }
 
     return "newPost";
