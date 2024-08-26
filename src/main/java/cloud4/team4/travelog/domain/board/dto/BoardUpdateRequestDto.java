2c42a9ff6a509102b11221b9eb145f46d855dc2e
thoon <131009655+ChatHTH@users.noreply.github.com>
2024-08-27 01:46:05 +0900

    fix: Update Dto 수정

--- src/main/java/cloud4/team4/travelog/domain/board/dto/BoardUpdateRequestDto.java
+++ src/main/java/cloud4/team4/travelog/domain/board/dto/BoardUpdateRequestDto.java
@@ -1,12 +1,20 @@
 package cloud4.team4.travelog.domain.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class BoardUpdateRequestDto {
     private String description;
+    private MultipartFile image;
+
+    public BoardUpdateRequestDto(String description, MultipartFile image) {
+        this.description = description;
+        this.image = image;
+    }
 }
