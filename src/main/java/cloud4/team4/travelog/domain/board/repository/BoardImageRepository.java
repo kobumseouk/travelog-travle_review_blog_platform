e3fd27372155a4628753181f327f8aa88039d47b
thoon <131009655+ChatHTH@users.noreply.github.com>
2024-08-27 19:18:50 +0900

    refactor: BoardImageRepository 미사용으로 주석 처리

--- src/main/java/cloud4/team4/travelog/domain/board/repository/BoardImageRepository.java
+++ src/main/java/cloud4/team4/travelog/domain/board/repository/BoardImageRepository.java
@@ -1,13 +1,13 @@
-package cloud4.team4.travelog.domain.board.repository;
-
-import cloud4.team4.travelog.domain.board.entity.BoardImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface BoardImageRepository extends JpaRepository<BoardImage, Long> {
-
-    List<BoardImage> findBoardImageByBoard_Id(Long id);
-}
+//package cloud4.team4.travelog.domain.board.repository;
+//
+//import cloud4.team4.travelog.domain.board.entity.BoardImage;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface BoardImageRepository extends JpaRepository<BoardImage, Long> {
+//
+//    List<BoardImage> findBoardImageByBoard_Id(Long id);
+//}
