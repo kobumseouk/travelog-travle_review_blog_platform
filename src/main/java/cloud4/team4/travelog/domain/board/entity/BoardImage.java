89f25fcb03fc3a30b32f26fbd5ba6272efdd4951
thoon <131009655+ChatHTH@users.noreply.github.com>
2024-08-27 17:07:48 +0900

    fix: BoardImage 주석 처리

--- src/main/java/cloud4/team4/travelog/domain/board/entity/BoardImage.java
+++ src/main/java/cloud4/team4/travelog/domain/board/entity/BoardImage.java
@@ -1,27 +1,27 @@
-package cloud4.team4.travelog.domain.board.entity;
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Getter
-@NoArgsConstructor
-public class BoardImage {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "board_photos_id")
-    private Long id;
-
-    private String imagePath;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "board_id")
-    private Board board;
-
-    public BoardImage(String imagePath, Board board) {
-        this.imagePath = imagePath;
-        this.board = board;
-    }
-}
+//package cloud4.team4.travelog.domain.board.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor
+//public class BoardImage {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "board_photos_id")
+//    private Long id;
+//
+//    private String imagePath;
+//
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "board_id")
+//    private Board board;
+//
+//    public BoardImage(String imagePath, Board board) {
+//        this.imagePath = imagePath;
+//        this.board = board;
+//    }
+//}
